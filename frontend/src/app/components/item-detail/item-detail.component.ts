f2bfcc0e12005c0e507a08b19dafe231ab2de58a
MartinNovysedlak <martinnovysedlak48@gmail.com>
2021-05-01 20:10:48 +0200

    detail

--- frontend/src/app/components/item-detail/item-detail.component.ts
+++ frontend/src/app/components/item-detail/item-detail.component.ts
@@ -3,8 +3,8 @@ import { Item } from '../../models/item';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common'
-import { User } from '../../models/user';
-
+import {UserService} from "../../services/user.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-item-detail',
@@ -14,12 +14,14 @@ import { User } from '../../models/user';
 export class ItemDetailComponent implements OnInit {
 
   item?: Item;
+  user?: User;
 
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private itemService: ItemService) { }
+    private itemService: ItemService,
+    private userService: UserService) { }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id')!;
