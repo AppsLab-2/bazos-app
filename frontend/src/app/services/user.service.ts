75a4e66462190f08d3cdea30b5e63d1d74c0af52
MartinNovysedlak <martinnovysedlak48@gmail.com>
2021-05-27 09:16:31 +0200

    CSS

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from "../models/user";
 import {REST_API} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {Item} from "../models/item";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,10 @@ export class UserService {
 
     addUser(user: User) {
       return this.http.post(REST_API + '/saveuser', user);
+  }
+
+  getUserInfo(email: string): Observable<Item> {
+    const url = `${REST_API}/detailuser/{email}`;
+    return this.http.get<Item>(url)
   }
 }
